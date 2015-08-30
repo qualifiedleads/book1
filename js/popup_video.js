25ad94f92823492cac77cdd5a5454775d6438f89
c3j333 <cjjsow@gmail.com>
2015-08-30 18:48:13 +0800

    Video Playback Updates
    Fixed centering
    Added player
    Added video samples
    More css

--- js/popup_video.js
+++ js/popup_video.js
@@ -3,22 +3,28 @@ popup_video = {
 	"init" : function(){
 		var popup_video_object = '<div id="popup_video_object"><div class="video-object-overlay"></div><div id="popup_video_object_box">Video Player</div></div>';
 		$("body").append(popup_video_object);
-		$("#popup_video_object_box").css({
+		var box = $("#popup_video_object_box");
+		var tm = box.height()+((box.outerHeight()-box.innerHeight())/2);
+		var lm = box.width()+((box.outerWidth()-box.innerWidth())/2);
+		box.css({
 	        'position' : 'absolute',
 	        'left' : '50%',
 	        'top' : '50%',
-	        'margin-left' : -$('#popup_video_object_box').width()/2,
-	        'margin-top' : -$('#popup_video_object_box').height()/2
+	        'margin-left' : -(lm/2),
+	        'margin-top' : -(tm/2)
 	    });
 	    $("#popup_video_object .video-object-overlay").click(popup_video.hide);
 	    var play_overlay = '<div class="video-thumb"><div class="background"></div><div class="foreground"></div></div>'
-	    $("div.img").click(popup_video.show).append(play_overlay);
+	    $("div.img").append(play_overlay).click(popup_video.show);
 	},
 	"show" : function(){
 		$("#popup_video_object_box iframe").attr("src","http://www.youtube.com/v/YE7VzlLtp");
 		$("#popup_video_object").css("display","block");
+		var videoID = $(this).attr("rel");
+		player.loadVideoById(videoID);
 	},
 	"hide" : function(){
 		$("#popup_video_object").css("display","none");
+		player.stopVideo();
 	}
 }
