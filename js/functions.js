416ae68f6175387c32d3c200de1ca59f4c925d17
mikertb <mike@rtb.cat>
2016-04-14 23:29:17 +0800

    Images & code optimization.

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,8 @@
-
 /*---------------------------
 LeadPages Custom Functions
 ----------------------------*/
 
-
 // Must load 3rd party API after data-attributes have been updated.
-
     /* Twitter */
     !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
     /* Google plus */
