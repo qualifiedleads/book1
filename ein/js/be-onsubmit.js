14667575a8b729124e1e4079d8f0912b549ed1bd
KurtLopez <kurtdevph@gmail.com>
2018-01-23 18:16:50 +0800

    add google conversion function to be-onsubmit.js

--- ein/js/be-onsubmit.js
+++ ein/js/be-onsubmit.js
@@ -50,7 +50,6 @@ $(document).ready(function(){
    }
 
  //   Ajax submit
-   
                $.ajax({
                    method: 'GET',
                    url: 'https://script.google.com/macros/s/AKfycbxQGaO8WWO578Tr9nw6NrWWOQ6F3ZC8xRjmyVOwr3_TEZOEsAq9/exec',
@@ -61,14 +60,18 @@ $(document).ready(function(){
                        $('#register_form input').prop("disabled",false);
                        $('#register_form select').prop("disabled",false);
                        $('#register_form button[type="submit"]').text("Wait...").prop("disabled",false);
-                     //   goog_report_conversion('https://www.oca.scientology.dk/questions.html');
+                       goog_report_conversion_1();
+                       goog_report_conversion_2();
+                       goog_report_conversion_3('https://dianeticsboek.nl/thank-you-extract/');
                    },
                    success: function(response) {
                        console.log('OCA started, OCA Id: ' + data.result);
                        $('#register_form input').prop("disabled",false);
                        $('#register_form select').prop("disabled",false);
                        $('#register_form button[type="submit"]').text("Wait...").prop("disabled",false);
-                     //   goog_report_conversion('https://www.oca.scientology.dk/questions.html');
+                       goog_report_conversion_1();
+                       goog_report_conversion_2();
+                       goog_report_conversion_3('https://dianeticsboek.nl/thank-you-extract/');
                    }
                });
    $('#register_form button[type="submit"]').text("Wait...").prop("disabled",true);
